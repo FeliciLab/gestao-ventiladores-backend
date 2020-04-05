44c34f21128d257cff5eb57b526455e245c9c628
Lucas Vieira <lucas_alves@atlantico.com.br>
2020-04-05 11:11:12 -0300

    evolucao no modelo do usuario

--- config/routes.py
+++ config/routes.py
@@ -1,5 +1,6 @@
-from views import equipamento_view
+from views import equipamento_view, usuario_view
 
 def initialize_routes(api):
     api.add_resource(equipamento_view.EquipamentoList, '/api/equipamentos')
     api.add_resource(equipamento_view.EquipamentoDetail, '/api/equipamentos/<id>')
+    api.add_resource(usuario_view.UsuarioList, '/api/usuarios')
