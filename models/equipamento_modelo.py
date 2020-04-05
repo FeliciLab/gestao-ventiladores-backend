f2671301a75f495e861aae266dd45dba002b411e
Lucas Vieira <lucas_alves@atlantico.com.br>
2020-04-05 12:15:01 -0300

    melhorias no modelo equipamento (ordem de servico)

--- models/equipamento_modelo.py
+++ models/equipamento_modelo.py
@@ -1,29 +1,25 @@
 from config.db import db
 
 
-class Equipamento(db.Document):
+class Equipamento(db.Document): # ordem de servico
     # FORMULARIO DE TRIAGEM DE EQUIPAMENTO
 
     numero_ordem_servico = db.StringField(required=True, unique=True)
 
-    foto_equipamento_chegada = db.StringField(
-        unique=True)  # verificar se tem um tipo especifico para links, faz sentido ser único ?
+    foto_equipamento_chegada = db.StringField()
     tipo = db.StringField(required=True)
     unidade_de_origem = db.StringField(required=True)
-    numero_do_patrimonio = db.IntField(required=True,
-                                       unique=True)  # verificar se é único mesmo, pois pelo o historico, pode existir varias vezes esse mesmo campo
-    numero_de_serie = db.StringField(required=True,
-                                     unique=True)  # verificar se é único mesmo, pois pelo o historico, pode existir varias vezes esse mesmo campo
+    numero_do_patrimonio = db.StringField(required=True)
+    numero_de_serie = db.StringField(required=True)
     instituicao_de_origem = db.StringField(required=True)
-    responsavel_contato_da_instituicao_de_origem = db.StringField(required=True)  # compo telefone é texto ou int ?
+    responsavel_contato_da_instituicao_de_origem = db.StringField(required=True)
     estado_de_conservacao = db.StringField(required=True)
     marca = db.StringField(required=True)
     modelo = db.StringField(required=True)
     acessorios = db.StringField(required=True)
     foto_apos_limpeza = db.StringField(required=True)
     observacao = db.StringField()
-    responsavel_pelo_preenchimento = db.StringField(
-        unique=True)  # verificar se tem um tipo especifico para links, faz sentido ser único ?
+    responsavel_pelo_preenchimento = db.StringField()
 
     # FORMULARIO DE DIAGNOSTICO CLINICO
     acoes_avaliacao = db.ListField(db.DictField(), required=True)
