6f9bf51269b3fd33ce8e7283acd955a3ed189295
Denis Menezes de Sousa <denismzsousa@gmail.com>
2020-04-11 18:27:47 -0300

    melhoria da validacao para cadastro de fabricante implementada

--- api/schemas/fabricante_schema.py
+++ api/schemas/fabricante_schema.py
@@ -2,12 +2,18 @@ from ..models import fabricante_model
 from marshmallow import Schema, fields
 
 
+class MarcaSchema(Schema):
+    class Meta:
+        model = fabricante_model.Fabricante
+        fields = ("marca", "modelos")
+
+    marca = fields.String(required=True)
+    modelos = fields.List(fields.String, required=True)
+
 class FabricanteSchema(Schema):
     class Meta:
         model = fabricante_model.Fabricante
-        #fields = ("fabricante_nome", "marcas", "modelos")
-        fields = ("fabricante_nome", "marcas", "modelos")
+        fields = ("fabricante_nome", "marcas")
 
     fabricante_nome = fields.String(required=True)
     marcas = fields.List(fields.Dict, required=True)
-    #modelos = fields.List(fields.String, required=True)
