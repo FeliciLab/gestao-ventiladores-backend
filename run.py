13c1044d573f319bc892bafd4738c32365605ce7
Denis Menezes de Sousa <denismzsousa@gmail.com>
2020-04-11 18:18:14 -0300

    .

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from config.routes import initialize_routes
 from flask_mongoengine import MongoEngine
+from env_config import mongodb_host
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,9 +10,10 @@ CORS(app)
 
 api = Api(app)
 
-app.config['MONGODB_SETTINGS'] = {
-    'host': 'mongodb://localhost/gestao-de-ventiladores'
-}
+DB_URI = mongodb_host
+
+app.config["MONGODB_HOST"] = DB_URI
+
 db = MongoEngine(app)
 initialize_routes(api)
 
