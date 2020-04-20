9304bc797220cd951d33a1bf45bc6d5e583e6e63
Denis Menezes de Sousa <denismzsousa@gmail.com>
2020-04-20 18:49:54 -0300

    ordem compra primeira versao

--- api/services/ordem_compra_service.py
+++ api/services/ordem_compra_service.py
@@ -1,27 +1,28 @@
-from api.models import ordem_compra
+from api.models import ordem_compra_model
 import random
 
 def listar_ordem_compras():
-    return ordem_compra.OrdemCompra.objects
+    return ordem_compra_model.OrdemCompra.objects
 
 
 def listar_ordem_compra_by_id(numero_ordem_compra):
     try:
-        return ordem_compra.OrdemCompra.objects.get(numero_ordem_compra=numero_ordem_compra)
+        return ordem_compra_model.OrdemCompra.objects.get(numero_ordem_compra=numero_ordem_compra)
     except:
         return None
 
 
 def listar_ordem_compra_by_numero_ordem_compra(numero_ordem_compra):
     try:
-        return ordem_compra.OrdemCompra.objects.get(numero_ordem_compra=numero_ordem_compra)
+        return ordem_compra_model.OrdemCompra.objects.get(numero_ordem_compra=numero_ordem_compra)
     except:
         return None
 
 
 def registar_ordem_compra(body):
-    print(body)
-    body["numero_ordem_compra"] = random.getrandbits(128)
-    return ordem_compra.OrdemCompra(**body).save()
+    qtd_ordem_compra = ordem_compra_model.OrdemCompra.objects.count()
+    numero_ordem_compra = str(qtd_ordem_compra + 1).zfill(4)
+    body['numero_ordem_compra'] = numero_ordem_compra
+    return ordem_compra_model.OrdemCompra(**body).save()
 
 
