c50a14a94f1796abd60168e68ec9685d84f0aff5
adrilene <adrilene.fonseca@aluno.uece.br>
2020-06-03 11:53:13 -0300

    [@adrilene/@chicaothiago/@denisousa] Issue #60 - Testes da rota get finalizados.

--- api/v2/controllers/items_many_controller.py
+++ api/v2/controllers/items_many_controller.py
@@ -1,24 +1,24 @@
 from flask_restful import Resource
-from flask import jsonify, request
+from flask import make_response, jsonify, request
 import json
-
 from ..helpers.error_response import error_response
 from ..services.item_service import ItemService
 
 
-def valid_deleted_paremeter(param):
-    return param == "true"
+def invalid_deleted_parameter(param):
+    return param and param != "true"
 
 class ItemsManyController(Resource):
     def get(self):
         args_deleted = request.args.get('deleted')
-        if not valid_deleted_paremeter(args_deleted):
+        if invalid_deleted_parameter(args_deleted):
             return error_response("Parameter deleted is not equal true.", 400)
 
-        pass
-        #
-        # if items_mongo is None:
-        #     return jsonify({"msg": "No items registered."}), 200
-        #
-        # items = json.loads(items_mongo.to_json())
-        # return jsonify(items), 200
+        content = ItemService.fetch_items_list()
+
+        response = {'content': content}
+
+        response['deleted'] = True if args_deleted else False
+
+        return make_response(jsonify(response), 200)
+  
--- tests/items_many/test_items_routes.py
+++ tests/items_many/test_items_routes.py
@@ -1,6 +1,7 @@
 import unittest
 from run import app
 from config.db import db
+from time import sleep
 
 class TestItemsRoutes(unittest.TestCase):
     def setUp(self):
@@ -13,4 +14,22 @@ class TestItemsRoutes(unittest.TestCase):
 
     def test_items_has_get_route(self):
         response = self.client.get('/v2/items')
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_items_if_there_is_parameter_deleted_is_not_equal_true(self):
+        response = self.client.get('/v2/pass?deleted=errei_no_trampo')
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("error", response.json)
+        self.assertEqual(response.json["error"], "Parameter deleted is not equal true.")
+
+    def test_get_items_if_there_is_parameter_deleted(self):
+        response_right = self.client.get('/v2/items?deleted=true')
+        self.assertEqual(response_right.status_code, 200)
+        self.assertIn('deleted', response_right.json)
+        self.assertEqual(response_right.json['deleted'], True)
+    
+    def test_get_items_if_there_is_not_parameter_deleted(self):
+        response_right = self.client.get('/v2/items')
+        self.assertEqual(response_right.status_code, 200)
+        self.assertIn('deleted', response_right.json)
+        self.assertEqual(response_right.json['deleted'],False)
