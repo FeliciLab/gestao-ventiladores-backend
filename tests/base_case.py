237d6a439a53706cadaf1801d0513468148101aa
adrilene <adrilene.fonseca@aluno.uece.br>
2020-06-16 14:43:22 -0300

    [@adrilene/@edely] Issue #70 - WIP iniciando formatação dos dados recuperados.

--- tests/base_case.py
+++ tests/base_case.py
@@ -4,6 +4,7 @@ from config.db import db
 from .mocks.mock_items import mock_items
 import json
 from copy import deepcopy
+from api.v2.migrations.itens_from_service_order import ItemsMigration
 
 
 class BaseCase(TestCase):
@@ -11,6 +12,7 @@ class BaseCase(TestCase):
         self.client = app.test_client()
         self.db = db.get_db()
         self.mock_items = mock_items
+        self.items_from_triagem = ItemsMigration().fetch_items_from_triagem()
 
     def tearDown(self):
         # Não será apagado para testar performance do BD
--- tests/items_many/test_items_migration.py
+++ tests/items_many/test_items_migration.py
@@ -1,13 +1,14 @@
-import ipdb; ipdb.set_trace()
 from tests.base_case import BaseCase
-from ...api.v2.migrations.itens_from_service_order import ItemsMigration
 
 
 class TestItemsMigration(BaseCase):
-    def test_items_from_triagem(self):
-        response = ItemsMigration().fetch_items_from_triagem()
+    def test_items_from_triagem_is_list(self):
+        response = self.items_from_triagem
         self.assertEqual(type(response), list)
+
+    def test_items_from_triagem_is_non_empty_list(self):
+        response = self.items_from_triagem
+        self.assertTrue(len(response) > 0)
 
     def test_items_has_formatted_names(self):
         pass
-
