f26365df144e488eedd74148fb8136d2dc8e2d04
Sarah Lima <lasarahlima@Sarahs-MacBook-Pro.local>
2020-06-25 14:29:14 -0300

    Remove unnecessary debugging code

--- api/v2/controllers/dtos/service_orders_request.py
+++ api/v2/controllers/dtos/service_orders_request.py
@@ -3,8 +3,5 @@ from .many_request import ManyRequest
 class ServiceOrdersRequest(ManyRequest):
     
     def validate(self):
-        print("EAI")
         super().validate()
-        print("valeu")
-        # here goes the service order specific validation
     
