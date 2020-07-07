acf3c69c470bec853e213d81c064e4da3a0c42e0
Edely Gomes <edelygomes@gmail.com>
2020-07-07 10:24:08 -0300

    [@edely] Issue #107 -  Adiciona validação via schema.

--- api/v2/controllers/items_merge_controller.py
+++ api/v2/controllers/items_merge_controller.py
@@ -4,6 +4,7 @@ from ..helpers.helper_response import error_response
 from ..validation.validation_request import (
     validate_merge_items_request
 )
+from ..validation.schemas.item_schema import ItemSchema
 
 class ItensMergeController(Resource):
 
@@ -12,6 +13,23 @@ class ItensMergeController(Resource):
         validate, message = validate_merge_items_request(body)
         if not validate:
             return error_response(message)
+
+        errors = {}
+
+        errors_toUpdate = ItemSchema().validate(body["content"]["toUpdate"])
+        if errors_toUpdate:
+            errors['errors_toUpdate'] = errors_toUpdate
+
+        errors_toRemove = {}
+        for index, item in enumerate(body["content"]["toRemove"]):
+            erro_schema = ItemSchema().validate(item)
+            if erro_schema:
+                errors_toRemove[str(index)] = erro_schema
+        if errors_toRemove:
+            errors['errors_toRemove'] = errors_toRemove
+
+        if errors:
+            return error_response(errors)
 
         return "", 201
 
